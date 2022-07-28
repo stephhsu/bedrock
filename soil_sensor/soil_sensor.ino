7acb78b92fb8f4f88ae4071e3bab2246ae32dc88
Stephanie Hsu <stephaniehsu@Stephanies-MacBook-Pro.local>
2022-07-28 01:15:11 -0400

    Logic fixes, collects data every three hours

--- soil_sensor/soil_sensor.ino
+++ soil_sensor/soil_sensor.ino
@@ -1,43 +1,84 @@
+#include <EEPROM.h>
 #include <RTClib.h>
-#include <soil_data.h>
+#include "soil_data.h"
+
+/* Phase 2 TODO:
+ * - research if it is possible to power off
+ */
+
+#define ADDR 0
+#define COUNT_ADDR 2
 
 RTC_DS1307 rtc;
 int eeAddress;
+int count;
+
+int lastHour = 0;
 
 void setup() {
   // open serial port, set the baud rate to 9600 bps
   Serial.begin(9600);
+  delay(1000);
 
-  if (! rtc.begin()) {
+  if (!rtc.begin()) {
    Serial.println("Couldn't find RTC");
    while (1);
-  }
-
-  EEPROM.get(0, eeAddress);
-  if (isnan(eeAddress)) {
-    // start storing data info at address 2
-    eeAddress = 2;
   }
 }
 
 void loop() {
-  int soilMoistureValue = analogRead(A0);
-  Serial.println(soilMoistureValue);
+  DateTime now = rtc.now();
+  if (now.hour() != lastHour) {
+    lastHour = now.hour();
+    if (now.hour() % 3 == 0) {
+      collectAndStoreSoilData();
+    }
+  }
+  
+  // TODO: check if bluetooth is in range
+  // TODO: call retrieveAndSendSoilData()
+}
 
+void collectAndStoreSoilData() {
+  // getting address where we can start storing
+   EEPROM.get(ADDR, eeAddress);
+  if (isnan(eeAddress)) {
+    // start storing data info at address 4
+    eeAddress = 4;
+  }
+
+  // getting current count of collected samples
+   EEPROM.get(COUNT_ADDR, count);
+  if (isnan(count)) {
+    count = 0;
+  }
+  
+  int soilMoistureValue = analogRead(A0);
+  
   // grabbing the current unix timestamp from rtc module
   DateTime now = rtc.now();
   uint32_t nowUnix = now.unixtime();
-
+  
   soil_data collected_data = {nowUnix, soilMoistureValue};
 
-  // store it in the EEPROM
-  EEPROM.put(eeAddress, collected_data);
-  eeAddress += sizeof(soil_data);
-  EEPROM.put(0, eeAddress);
+  // store collected data in the EEPROM
+  EEPROM.put(eeAddress+(count*sizeof(soil_data)), collected_data);
 
-  // TODO: check if bluetooth is in range
-  // TODO: send data the data over bluetooth
-  // TODO: clear the data from EEPROM
-  // TODO: research if it is possible to power off instead of delay
-  delay(250);
+  // increase the count and store it in EEPROM
+  count++;
+  EEPROM.put(COUNT_ADDR, count);
+}
+
+void retrieveAndSendSoilData() {
+  EEPROM.get(ADDR, eeAddress);
+  EEPROM.get(COUNT_ADDR, count);
+
+  for (int i=0; i<count; i++) {
+    soil_data retrieved_data;
+    EEPROM.get(eeAddress, retrieved_data);
+    // TODO: send via serial over bluetooth
+  }
+
+  // reset the count back to 0
+  EEPROM.put(COUNT_ADDR, 0);
 }
