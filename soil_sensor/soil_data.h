a97fc0c398f369ca87818ba578ed5ed8a27ec83e
Stephanie Hsu <steph.c.hsu@gmail.com>
2023-03-13 23:34:01 -0400

    added bluetooth logic to soil sensor code

--- soil_sensor/soil_data.h
+++
@@ -1,9 +0,0 @@
-struct soil_data {
-  uint32_t unix_timestamp;
-  int moisture_value;
-};
-
-union outputToBt {
-  soil_data data;
-  byte btLine[6];
-};
--- soil_sensor/soil_sensor.ino
+++ soil_sensor/soil_sensor.ino
@@ -1,9 +1,21 @@
 #include <EEPROM.h>
 #include <RTClib.h>
-#include "soil_data.h"
+#include <SoftwareSerial.h>
 
 #define ADDR 0
 #define COUNT_ADDR 2
+
+SoftwareSerial BTSerial(2, 3); // RX | TX
+
+struct soil_data {
+  uint32_t unix_timestamp;
+  int moisture_value;
+};
+
+union outputToBT {
+  soil_data data;
+  byte BTLine[6];
+};
 
 outputToBt outputData;
 byte btData[6];
@@ -13,20 +25,27 @@ RTC_DS1307 rtc;
 int eeAddress;
 int count;
 
+int inc = 0;
 int lastHour = -1;
 
 void setup() {
   // open serial port, set the baud rate to 9600 bps
   Serial.begin(38400);  // might be 9600
-  Serial.setTimeout(10);
-  delay(1000);
+  Serial.println("SLAVE MODULE 1");
 
-  // HC-05 default serial speed for AT mode is 38400
-  Serial1.begin(38400); 
+  BTSerial.begin(38400);
 
   if (!rtc.begin()) {
    Serial.println("Couldn't find RTC");
    while (1);
+  }
+
+  Serial.println("clearing EEPROM");
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+  for (int i = 0; i < 1; i++) {
+    collectAndStoreSoilData();
   }
 }
 
@@ -34,41 +53,58 @@ void loop() {
   DateTime now = rtc.now();
   if (now.hour() != lastHour) {
     lastHour = now.hour();
-    if (now.hour() % 3 == 0) {
+    if (now.hour() % 3 == 0) { // set to every 3 hours. change to 1 hour for demo
       collectAndStoreSoilData();
     }
   }
 
   // data incoming from bluetooth module
-  if (Serial1.available()) {
-    c = Serial1.read();
-    if (c == 'h') {
-      retrieveAndSendSoilData();
+  if (BTSerial.available()) {  
+    c = BTSerial.read();
+    if (c == 'm') {
+
+    EEPROM.get(ADDR, eeAddress);
+    EEPROM.get(COUNT_ADDR, count);
+
+    // let rover know in advance the data count
+    BTSerial.write(count);
+    delay(5000);
+
+    for (int i=0; i<count; i++) {
+      EEPROM.get(eeAddress+(i*sizeof(soil_data)), outputData.data);
+      sendSoilData();
+      delay(2000);
     }
-  }
+
+    //EEPROM.put(COUNT_ADDR, 0);
+    } else {
+      Serial.print("received");
+      Serial.println(c);
+    }
+  }  
 }
 
 void collectAndStoreSoilData() {
   // getting address where we can start storing
   EEPROM.get(ADDR, eeAddress);
-  if (isnan(eeAddress)) {
+  if (isnan(eeAddress) || eeAddress == 0 ) {
     // start storing data info at address 4
     eeAddress = 4;
   }
 
   // getting current count of collected samples
   EEPROM.get(COUNT_ADDR, count);
+  Serial.print("count: "); Serial.println(count);
   if (isnan(count)) {
     count = 0;
   }
   
   int soilMoistureValue = analogRead(A0);
-  
-  // grabbing the current unix timestamp from rtc module
-  DateTime now = rtc.now();
+  DateTime now = rtc.now(); // grabbing the current unix timestamp from rtc module
   uint32_t nowUnix = now.unixtime();
   
   soil_data collected_data = {nowUnix, soilMoistureValue};
+  inc++;
 
   // store collected data in the EEPROM
   EEPROM.put(eeAddress+(count*sizeof(soil_data)), collected_data);
@@ -78,22 +114,6 @@ void collectAndStoreSoilData() {
   EEPROM.put(COUNT_ADDR, count);
 }
 
-void retrieveAndSendSoilData() {
-  EEPROM.get(ADDR, eeAddress);
-  EEPROM.get(COUNT_ADDR, count);
-
-  // let rover know in advance the data count
-  Serial1.write(count);
-
-  for (int i=0; i<count; i++) {
-    EEPROM.get(eeAddress+(i*sizeof(soil_data)), outputData.data);
-    sendSoilData();
-  }
-
-  // reset the count back to 0
-  EEPROM.put(COUNT_ADDR, 0);
-}
-
 void sendSoilData () {
   for (byte n = 0; n < 6; n++) {
      BTData[n] = outputData.BTLine[n];
@@ -101,6 +121,6 @@ void sendSoilData () {
    
   for (byte n = 0; n < 6; n++) {
       Serial.print("Sending:   "); Serial.println(BTData[n]);
-      Serial1.write(BTData[n]);
+      BTSerial.write(BTData[n]);
    }
 }
