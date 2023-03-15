fb11bcea84d680c6668777b9eb126e5204b3539b
Nina Zhao <nina.zhao@smartex.ai>
2023-03-15 16:23:36 -0400

    send start command

--- data_transfer_nodemcu/data_transfer_nodemcu.ino
+++ data_transfer_nodemcu/data_transfer_nodemcu.ino
@@ -27,7 +27,9 @@ inputFromBT inputData[2];
 byte BTData[6];
 const char *ssid =  "ninanina";     // replace with your wifi ssid and wpa2 key
 const char *pass =  "galileo2019";
-String baseUrl = "http://18.217.62.57:4000";
+String baseUrl = "http://3.133.160.82:4000";
+bool startCommandReceived = false;
+bool startCommandSentToMega = false;
 
 void setup() {
   Serial.begin(9600);
@@ -37,54 +39,99 @@ void setup() {
   Serial.println(ssid); 
  
   WiFi.begin(ssid, pass); 
-  while (WiFi.status() != WL_CONNECTED) 
-    {
-      delay(500);
-      Serial.print(".");
-    }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  connectToWifi();
 
   soilSensorBoard.begin(9600); // use previous rxPin, txPin and set 256 RX buffer. connect to soil sensor board
   Serial.println(F("ESP8266 Setup finished."));
 }
 
 void loop() {
-  if (dataReceived == false) {
-    soilSensorBoard.write('s');
-    while (!soilSensorBoard.available()) {
-      Serial.println("No data from arduino yet");
-      delay(5000);
-    }
-    for (byte i = 0; i < 2; i++) {
-      receiveData(i);
-    }
-    displayData();
-    String json1 = formatSensorData(0);
-    String json2 = formatSensorData(1);
-    Serial.println(".");
-    Serial.println(".");
-    Serial.println(".");
+  if (!startCommandReceived) {
+    checkForStartCommand();
+  } else if (!startCommandSentToMega) {
+    Serial.println("Sending start char");
+    soilSensorBoard.write('t');
+    startCommandSentToMega = true;
+    // while (!soilSensorBoard.available()) {
+    //   Serial.println("No data from arduino yet");
+    //   Serial.println(".");
+    //   delay(5000);
+    // }
+    // for (byte i = 0; i < 2; i++) {
+    //   receiveData(i);
+    // }
+    // Serial.println("delay before displaying");
+    // delay(3000);
+    // displayData();
+
+    // //format data into json for sending to api
+    // String json1 = formatSensorData(0);
+    // String json2 = formatSensorData(1);
+    // Serial.println("Checking wifi connection");
+    // connectToWifi();
     // sendDataToAPI();
   }
   delay(5000);
 }
 
+void connectToWifi() {
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void checkForStartCommand() {
+  while (!startCommandReceived) {
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+
+      String url = baseUrl + "/rover/cycle-status";
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, url.c_str());
+      // http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        if (httpResponseCode == 204) {
+          startCommandReceived = true;
+        }
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    delay(10000);
+  }
+}
+
 void receiveData(byte i) {
   if (soilSensorBoard.available() < 6) {
-      return;
-    }
-    for (byte n = 0; n < 6; n++) {
-      BTData[n] = soilSensorBoard.read();
-    }
-    
-    for (byte n = 0; n < 6; n++) {
-      inputData[i].BTLine[n] = BTData[n];
-      Serial.print("<Receiving BTLine ... ");
-      Serial.println(inputData[i].BTLine[n]);
-    }
-    soilSensorIds[i] = soilSensorBoard.read(); // last char of data received is the sensor id
-    delay(2000);
+    return;
+  }
+  for (byte n = 0; n < 6; n++) {
+    BTData[n] = soilSensorBoard.read();
+  }
+  
+  for (byte n = 0; n < 6; n++) {
+    inputData[i].BTLine[n] = BTData[n];
+    Serial.print(n);
+    Serial.print("<Receiving BTLine ... ");
+    Serial.println(inputData[i].BTLine[n]);
+  }
+  soilSensorIds[i] = soilSensorBoard.read(); // last char of data received is the sensor id
+  delay(7000);
 }
 
 void displayData() {
@@ -117,7 +164,7 @@ void sendDataToAPI() {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
         String payload = http.getString();
-        Serial.println(payload);
+        // Serial.println(payload);
       }
       else {
         Serial.print("Error code: ");
@@ -146,6 +193,9 @@ String formatJSON() {
 }
 
 String formatSensorData(byte i) {
-  String result = String("{\"sensor_id\": \" ") + soilSensorIds[i] + String("\",\"sensor_data\": [{\"timestamp\": ") + inputData[i].data.unix_timestamp + String(",\"value\": ") + inputData[i].data.moisture_value + String("}]}");
-  return result;
+  if (i == 1) {
+    return String("{\"sensor_id\": \"a\",\"sensor_data\": [{\"timestamp\": \"1678507857\",\"value\": \"450\"}]}");
+  } else {
+    return String("{\"sensor_id\": \"b\",\"sensor_data\": [{\"timestamp\": \"1678509505\",\"value\": \"550\"}]}");
+  }
 }
